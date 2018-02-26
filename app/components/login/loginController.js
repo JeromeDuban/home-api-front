d71e71e74298f130039ab8d0f57a762c2e5d92dd
Jérôme Duban <jduban@enseirb-matmeca.fr>
2018-02-27 00:19:50 +0100

    check added on login form

--- app/components/login/loginController.js
+++ app/components/login/loginController.js
@@ -2,5 +2,16 @@ app.controller('loginController', ['$scope', function($scope) {
   $scope.validate = function() {
         console.log($scope.login);
         console.log($scope.password);
+        if(!$scope.login){
+        	$scope.message = "Merci de rentrer un nom d'utilisateur."
+        	return;
+        }
+
+        if (!$scope.password){
+        	$scope.message = "Merci de rentrer un mot de passe."
+        	return;
+        }
+        	
+
     };
 }]);
