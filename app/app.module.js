dc5b09132b9d232a902d3c4174805fcfa38f5a09
Jérôme Duban <jduban@enseirb-matmeca.fr>
2018-02-27 23:40:45 +0100

    authentication service added

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,1 @@
-var app = angular.module('homeAPI', ['ngRoute']);
+var app = angular.module('homeAPI', ['ngRoute','ngStorage']);
--- app/app.routes.js
+++ app/app.routes.js
@@ -6,7 +6,24 @@ app.config(function($routeProvider) {
         })
         .when("/login", {
             templateUrl : "app/components/login/loginView.html",
-            controller : "loginController"
+            controller : "loginController",
+            controllerAs : "vm"
         });
     }
-);
+).run(run);
+
+function run($rootScope, $http, $location, $localStorage) {
+    // keep user logged in after page refresh
+    if ($localStorage.currentUser) {
+        $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.currentUser.token;
+    }
+
+    // redirect to login page if not logged in and trying to access a restricted page
+    $rootScope.$on('$locationChangeStart', function (event, next, current) {
+        var publicPages = ['/login'];
+        var restrictedPage = publicPages.indexOf($location.path()) === -1;
+        if (restrictedPage && !$localStorage.currentUser) {
+            $location.path('/login');
+        }
+    });
+}
